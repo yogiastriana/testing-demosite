c1cabfe5c50f6b429f6f01d8c62d99cd2623977d
Yogi Astriana <yogiastriyana123@gmail.com>
2025-01-30 22:48:14 +0700

    Update project structure and templates for MVOH
    Menambahkan dan memperbarui URL, views, dan permulation di mvoh.
    Menyesuaikan template untuk index.html dan input_form.html di mvoh.
    Memperbarui template dashboard.html di permutation-mvoh.
    Memperbarui bagian template-parts dengan card.html dan modals.html.
    Menyempurnakan base.html untuk konsistensi layout.

--- mvoh/urls.py
+++ mvoh/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import input_form_view, output_view, run_mvoh_model_view, current_mvoh_result_view, fetch_mvoh_error_view, fetch_mvoh_ticker_view
+from .views import permutation_dashboard_view, input_form_view, get_sector_industries_view, get_industries_view, get_mvoh_metric_data_view, run_mvoh_model_view, current_mvoh_result_view, output_view
 
 urlpatterns = [
-    path('input/', input_form_view, name="mvoh-input"),
-    path('mvoh-result/', output_view, name="mvoh-result"),
-    path('run-mvoh-model/', run_mvoh_model_view, name="run-mvoh-model"),
+    path('', permutation_dashboard_view, name="permutation-mvoh"),
+    path('input/', input_form_view, name="mvoh"),
+    path('p1-result/', output_view, name="p1-result"),  
+    path('get-sector-industries/', get_sector_industries_view, name="get-sector-industries"),
+    path('get-industries/', get_industries_view, name="get-industries"),
+    path('fetch-mvoh-metric-data/', get_mvoh_metric_data_view, name="fetch-mvoh-metric-data"),
+    path('run-mvoh/', run_mvoh_model_view, name="run-mvoh"),
     path('current-mvoh-result/', current_mvoh_result_view, name="current-mvoh-result"),
-    path('fetch-mvoh-error/', fetch_mvoh_error_view, name='fetch-mvoh-error'),
-    path('fetch-ticker-data/', fetch_mvoh_ticker_view, name='fetch-mvoh-tciker'),
 ]
